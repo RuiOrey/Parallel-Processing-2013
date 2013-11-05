8a4b8081af98fc42cc11e532efef17e28a88b377
RuiOrey <rui.albuquerque.dorey@gmail.com>
2013-11-05 01:33:52 +0000

    Damping working and convergence working but buggy

--- Work1/PageRank.c
+++ Work1/PageRank.c
@@ -4,10 +4,14 @@
 #include <assert.h>
 
 #include <mpi.h>
+#include <stdbool.h>
+#include <math.h>
 
 static double *transition_matrix = NULL;
+//static double *damping_matrix = NULL;
 static double *pagerank_vector = NULL;
 static int size_graph = 0;
+
 
 // since we dynamically allocate our matrix data in a contiguous region
 // of memory, we need to use this macro to access a position of the matrix
@@ -63,12 +67,12 @@ allocate_pagerank_vector(void)
 }
 
 static void
-use_dummy_graph(void)
+use_dummy_graph(double damping)
 {
    size_graph = 4;
 
    const double initial_pagerank = 1.0 / (double)size_graph;
-   int i;
+   int i,j;
 
    allocate_pagerank_vector();
    transition_matrix = (double*)malloc(sizeof(double)*size_graph*size_graph);
@@ -76,6 +80,8 @@ use_dummy_graph(void)
    for(i = 0; i < size_graph; ++i) {
       pagerank_vector[i] = initial_pagerank;
    }
+
+
 
    transition_matrix[POS(0, 0)] = 0.0;
    transition_matrix[POS(0, 1)] = 0.0;
@@ -93,6 +99,26 @@ use_dummy_graph(void)
    transition_matrix[POS(3, 1)] = 1.0/2.0;
    transition_matrix[POS(3, 2)] = 0.0;
    transition_matrix[POS(3, 3)] = 0.0;
+
+   
+   //damping variables
+   const double damping_value=1*initial_pagerank*damping;
+   const double damping_inverse=1-damping;
+   // Generates matrix for dumping - maybe not needed full matrix
+   //damping_matrix = (double*)malloc(sizeof(double)*size_graph*size_graph);
+   for (i=0;i<size_graph;i++){
+      for (j=0;j<size_graph;j++){
+         printf("Position %d %d : %f\n",i,j,transition_matrix[POS(i,j)]);
+         transition_matrix[POS(i,j)]=transition_matrix[POS(i,j)]*damping_inverse + damping_value; 
+         printf("Position %d %d : %f\n",i,j,transition_matrix[POS(i,j)]);
+      }
+   }
+
+   
+
+  
+
+
 }
 
 int
@@ -118,7 +144,8 @@ main(int argc, char **argv)
    }
 
    int rank, size;
-
+   bool converge= false;
+   
    MPI_Init(&argc, &argv);
    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
    MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -131,7 +158,7 @@ main(int argc, char **argv)
       printf("Iterations: %d\n", iterations);
 
       // read_file(file);
-      use_dummy_graph();
+      use_dummy_graph(damping);
       print_transition_matrix();
       print_pagerank_vector();
 
@@ -140,6 +167,7 @@ main(int argc, char **argv)
    }
 
    MPI_Bcast(&size_graph, 1, MPI_INT, 0, MPI_COMM_WORLD);
+   MPI_Bcast(&converge, 1, MPI_C_BOOL, 0, MPI_COMM_WORLD);
 
    // non zero processes have not allocated their pagerank vector yet
    if(rank != 0)
@@ -149,35 +177,73 @@ main(int argc, char **argv)
    const int rows = size_graph / size;
 
    // transmit pagerank vector
-   int temp_iterator;
+   int temp_iterator=0;
    double *submatrix = (double *)malloc(sizeof(double)*size_graph * rows);
    double *partial_pagerank_vector = (double *)malloc(sizeof(double) * rows);
 
 
    // cycle for iterators 
-   for(temp_iterator=0;temp_iterator<iterations;temp_iterator++)
+
+   while(temp_iterator<iterations)
    { 
+      
       MPI_Bcast(pagerank_vector, size_graph, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-
-
+      /*
+      MPI_Scatter involves a designated root process sending data to all processes in a communicator. 
+      The primary difference between MPI_Bcast and MPI_Scatter is small but important. 
+      MPI_Bcast sends the same piece of data to all processes while MPI_Scatter sends chunks of an 
+      array to different processes. Check out the illustration below for further clarification.
+      */
+      int converge_temp=1;
       MPI_Scatter(transition_matrix, rows*size_graph, MPI_DOUBLE, submatrix, rows*size_graph,
          MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
+      int numbers=0;
    // multiply submatrix
       int i, j;
       for(i = 0; i < rows; ++i) {
+         
          double acc = 0.0;
          for(j = 0; j < size_graph; ++j) {
             acc += submatrix[POS(i, j)] * pagerank_vector[j];
          }
          partial_pagerank_vector[i] = acc;
+
+         //covergence test - working by reduce
+         converge_temp = converge_temp && (fabs(pagerank_vector[numbers+rows*rank]-acc)<0.0000009);
+         
+         // debug messages
+         printf("acc:%f item:%d rank:%d before:%f difference:%f boolean :%d converge_temp: %d \n",acc,numbers+rows*rank,rank,pagerank_vector[numbers+rows*rank],fabs(pagerank_vector[numbers+rows*rank]-acc),fabs(pagerank_vector[numbers+rows*rank]-acc)<0.0000009,converge_temp);
+         numbers++;
       }
 
+      /*
+      MPI_Gather is the inverse of MPI_Scatter. 
+      Instead of spreading elements from one process to many processes, MPI_Gather takes elements 
+      from many processes and gathers them to one single process. This routine is highly useful to 
+      many parallel algorithms, such as parallel sorting and searching.
+      */
+      //used for converge
+      
+      
       MPI_Gather(partial_pagerank_vector, rows, MPI_DOUBLE, pagerank_vector, rows, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-      if(rank == 0)
+      
+      if(rank == 0){
          print_pagerank_vector();
+         
+      }
+      temp_iterator++;
+      MPI_Barrier(MPI_COMM_WORLD);
+      MPI_Reduce(&converge_temp, &converge, 1,MPI_INT, MPI_PROD,0,MPI_COMM_WORLD);
+
+      if (converge==1) {
+         printf("Values Converged at iteration %d!\n",temp_iterator);
+         temp_iterator=iterations; 
+      }
+
    }
+
+   printf("here");
    MPI_Finalize();
 
    // free dynamic memory
@@ -185,7 +251,8 @@ main(int argc, char **argv)
    free(partial_pagerank_vector);
    free(pagerank_vector);
    if(rank == 0)
-      free(transition_matrix);
+      {free(transition_matrix);
+      }
 
    return EXIT_SUCCESS;
 }
